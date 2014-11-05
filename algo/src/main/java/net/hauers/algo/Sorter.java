653b99e56b4d020bcddd354147db589e96729867
Tamás Hauer <Tamas.Hauer@prodema.ch>
2014-11-05 13:20:16 +0100

    ...

--- algo/src/main/java/net/hauers/algo/Sorter.java
+++ algo/src/main/java/net/hauers/algo/Sorter.java
@@ -1,5 +1,46 @@
 package net.hauers.algo;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Sorter {
 
+  public static void qsort(List<? extends Comparable> list) {
+    qsort(list, 0, list.size()-1);
+  }
+
+  public static <T extends Comparable<? super T>> void qsort(List<T> list, int start, int end) {
+
+    // int pivot = start;
+
+    // int left = start;
+    // int right = end;
+
+    // for(; list.get(left).compareTo(list.get(pivot))<0; left++);
+    
+    // swap(list, left, pivot);
+    // int tmp = pivot
+    // int pivot = left
+    // int left = tmp
+
+    // for(; list.get(right).compareTo(list.get(pivot))>0; right--);
+    
+    // swap(list, right, pivot);
+    // int tmp = pivot
+    // int pivot = right
+    // int right = tmp
+
+  }
+
+
+  public static <T extends Comparable<? super T>> int walkleft(List<T> list, int start, int end) {
+    int pivot = start;
+    
+  }
+
+  public static <T> void swap(List<T> list, int i1, int i2) {
+    T temp = list.get(i1);
+    list.set(i1, list.get(i2));
+    list.set(i2, temp);
+  }
 }
--- algo/src/test/groovy/net/hauers/algo/SorterSpec.groovy
+++ algo/src/test/groovy/net/hauers/algo/SorterSpec.groovy
@@ -1,7 +1,41 @@
 package net.hauers.algo
 
-import spock.lang.Specification
+import spock.lang.*
 
 class SorterSpec extends Specification {
 
+  def "Sorter sorts"() {
+
+    given: "A list"
+    def list = [3, 2, 1]
+
+    when: "Calling qsort with the list"
+    Sorter.qsort(list)
+
+    then: "The list is sorted"
+    (0..list.size-1).every{list[it] < list[it+1]}
+  }
+
+  def "Partial sorter sorts"() {
+
+    given: "A list"
+    def list = [3, 2, 7, 5, 6, 5, 8, 0, 3, 4, 9]
+
+    when: "Calling qsort with the list"
+    Sorter.qsort(list, 2, 6)
+
+    then: "The list is partially sorted"
+    list == [3, 2, 5, 5, 6, 7, 8, 0, 3, 4, 9]
+  }  
+
+  def "Swap swaps"() {
+    given:
+    def list = [1, 2, 3, 4]
+
+    when:
+    Sorter.swap(list, 1, 3)
+
+    then:
+    list == [1, 4, 3, 2]
+  }
 }
