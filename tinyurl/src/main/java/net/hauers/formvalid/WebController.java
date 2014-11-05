660f6090aa11a5f5eb25e999b8f41cb6008844f1
Tamás Hauer <Tamas.Hauer@mcmri.com>
2014-11-05 01:08:12 +0100

    Person domain class

--- tinyurl/src/main/java/net/hauers/formvalid/WebController.java
+++ tinyurl/src/main/java/net/hauers/formvalid/WebController.java
@@ -2,13 +2,24 @@ package net.hauers.formvalid;
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Controller
 public class WebController extends WebMvcConfigurerAdapter {
-    
-  @RequestMapping("**")
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/results").setViewName("results");
+  }
+
+  @RequestMapping(value="/", method=RequestMethod.GET)
+  public String showForm(Person person) {
+    return "form";
+  }
+  
   @ResponseBody
   public String showForm() {
     return "hello\n";
