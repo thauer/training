69d2db4dee8cc0fa6fd51d5f69611501f9596fe9
Tamás Hauer <Tamas.Hauer@prodema.ch>
2014-11-12 17:23:10 +0100

    Gradle daemon, URLs fixed

--- aws/deptest.groovy
+++ aws/deptest.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy 
 
-@GrabResolver(name='thauer', root='https://dl.dropboxusercontent.com/u/23240137/maven-repository', m2Compatible='true')
+@GrabResolver(name='thauer', root='http://bit.ly/thauer-aws-maven', m2Compatible='true')
 @Grab('thauer:aws:0.1.0')
 import net.hauers.aws.SimpleAws
 
--- aws/src/main/java/net/hauers/aws/package-info.java
+++ aws/src/main/java/net/hauers/aws/package-info.java
@@ -1,22 +1,21 @@
 /**
  * Amazon AWS Convenience Wrappers for EC2<p>
  * This is a thin wrapper on the AWS SDK for Java.
- * <p>Mostly developed as a coding exercise</p>
+ * <p>Mostly developed as a [coding exercise](http://bit.ly/thauer-aws-github)</p>
  * <h3>Distribution</h3>
  * <p>Have not thought about license, thus there isn't one yet.
  *
  * <h3>Example</h3>
- * The following groovy script is included and illustrates the kind of things this is good for
+ * The following groovy script is included and illustrates how this is meant to be used:
  *
  * <pre>
  * #!/usr/bin/env groovy 
- * &#64;GrabResolver(name='thauer', 
- *   root='https://dl.dropboxusercontent.com/u/23240137/maven-repository',
- *   m2Compatible='true')
+ * &#64;GrabResolver(name='thauer', root='http://bit.ly/thauer-aws-maven')
  * &#64;Grab('thauer:aws:0.1.0')
  * import net.hauers.aws.SimpleAws
  * 
- * println new SimpleAws().querySingleInstance("i-59185b19") * 
+ * println new SimpleAws().querySingleInstance("i-59185b19")
+ *
  * </pre>
  */
 package net.hauers.aws;
