9faf41b35efa3f0ab7cb2c35fefd91559feed093
Tamás Hauer <Tamas.Hauer@prodema.ch>
2014-12-17 09:19:39 +0100

    waitingforbus

---
+++ EclipseCoder/waitingforbus-java/WaitingForBus.java
@@ -0,0 +1,14 @@
+public class WaitingForBus {
+
+	/**
+	 * 1 <= N <= 100
+	 * @param time array of N integers, each between 1 and 100_000, inclusive
+	 * @param prob array of N integers, each between 1 and 100, inclusive, sum is 100
+	 * @param s: current time between 0 and 100_000, inclusive
+	 * @return
+	 */
+  public double whenWillBusArrive(int[] time, int[] prob, int s) {
+		return 0.0;
+	}
+
+}
