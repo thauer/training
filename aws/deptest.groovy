35f522f018527e7589bb77539ae1b11c2b497070
Tamás Hauer <Tamas.Hauer@prodema.ch>
2015-03-10 08:50:43 +0100

    Removes aws

--- aws/deptest.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy 
-
-@GrabResolver('https://dl.dropboxusercontent.com/u/23240137/maven-repository')
-@GrabConfig(systemClassLoader = true)
-@Grab('thauer:aws:0.1.0')
-import net.hauers.aws.SimpleAws
-
-println new SimpleAws().querySingleInstance("i-59185b19")
-
